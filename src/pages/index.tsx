901a4832df46df932e0af3c59aeb47236234994d
dyslaxic <3xdevvv@gmail.com>
2024-12-31 14:06:14 +0000

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,40 @@
+import clsx from 'clsx';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import Heading from '@theme/Heading';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero', styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          <span
+            style={{
+              color: 'transparent',
+              backgroundClip: 'text',
+              backgroundImage: `linear-gradient(to right, #60a5fa, #a78bfa)`,
+            }}
+          >{siteConfig.title}</span>
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+
+      </main>
+    </Layout>
+  );
+}
